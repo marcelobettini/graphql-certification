35943084f2c079a9120b658eb95e4dd38986ffc9
Marcelo_Bettini <marcelobettini@yahoo.com.ar>
2024-06-07 15:48:38 -0300

    naming port

--- server/src/index.js
+++ server/src/index.js
@@ -6,7 +6,7 @@ const { startStandaloneServer } = require("@apollo/server/standalone");
 const typeDefs = require("./schema.js");
 const resolvers = require("./resolvers.js");
 const TrackAPI = require("./resolvers.js");
-const port = process.env.PORT ?? 4000;
+const port = process.env.port ?? 4000;
 async function startApolloServer() {
   const server = new ApolloServer({ typeDefs, resolvers });
 
